007992e4622a4896b29aba38f1de3483b47a354d
liuduwei <17341730328@163.com>
2025-01-02 17:22:02 +0800

    fix: acme

--- example/lib/screens/main/pages/downloading/components/main_statistics.dart
+++ example/lib/screens/main/pages/downloading/components/main_statistics.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 import 'package:provider/provider.dart';
 
